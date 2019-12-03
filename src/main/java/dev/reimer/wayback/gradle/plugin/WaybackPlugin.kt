963802f4db3c127809a6712b35c258788c213d30
Jan Heinrich Reimer <heinrich@heinrichreimer.com>
2019-12-03 03:34:57 +0100

    Add nop plugin implementation

---
+++ src/main/java/dev/reimer/wayback/gradle/plugin/WaybackPlugin.kt
@@ -0,0 +1,10 @@
+package dev.reimer.wayback.gradle.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class WaybackPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        // Do nothing.
+    }
+}
